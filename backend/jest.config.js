031ba918d3af15b9681bbbbcf8aa7edc75e9275f
Anderson Rodrigues <eusouanderson@outlook.com>
2025-07-23 10:07:32 -0300

    test: add TDD test for creating a contact

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,16 @@
-// jest.config.mjs
 import { defaults } from 'jest-config';
 
 export default {
+  preset: 'ts-jest',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testEnvironment: 'node',
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  
+  testMatch: ['**/?(*.)+(test|spec).ts?(x)'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json', 
+    },
+  },
 };
